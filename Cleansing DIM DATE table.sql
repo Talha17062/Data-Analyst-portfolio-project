4874035189e4ad9d1e0a1a23b029960d494ccdf5
Talha17062 <142256921+Talha17062@users.noreply.github.com>
2024-02-06 18:20:27 +0500

    Add files via upload

---
+++ Cleansing DIM DATE table.sql
@@ -0,0 +1,27 @@
+-- Cleansing DimDate Table
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date 
+  -- ,[DayNumberOfWeek]
+  ,[EnglishDayNameOfWeek] AS Day 
+  -- ,[SpanishDayNameOfWeek]
+  -- ,[FrenchDayNameOfWeek]
+  -- ,[DayNumberOfMonth] 
+  -- ,[DayNumberOfYear]
+  -- ,[WeekNumberOfYear]
+  , 
+  [EnglishMonthName] AS Month 
+  -- ,[SpanishMonthName]
+  -- ,[FrenchMonthName]
+  , 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  -- ,[CalendarSemester]
+  -- ,[FiscalQuarter]
+  -- ,[FiscalYear]
+  -- ,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate] 
+WHERE 
+  CalendarYear >= 2019
